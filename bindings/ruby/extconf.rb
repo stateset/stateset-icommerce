415c87fd6ba347782f7629ced6e8eb04c3234610
domsteil <domsteil14@gmail.com>
2026-04-29 08:05:30 -0700

    ci: configure ruby extension cargo manifest

--- bindings/ruby/extconf.rb
+++ bindings/ruby/extconf.rb
@@ -3,4 +3,6 @@
 require 'mkmf'
 require 'rb_sys/mkmf'
 
-create_rust_makefile('stateset_embedded/stateset_embedded')
+create_rust_makefile('stateset_embedded/stateset_embedded') do |rust|
+  rust.features = %w[runtime]
+end
