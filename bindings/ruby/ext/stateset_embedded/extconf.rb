d67b1e2a62e6f39fa6cbc17c37cc968427f3723c
domsteil <domsteil14@gmail.com>
2026-04-29 08:08:01 -0700

    ci: fix ruby nested extension path

--- bindings/ruby/ext/stateset_embedded/extconf.rb
+++ bindings/ruby/ext/stateset_embedded/extconf.rb
@@ -2,6 +2,6 @@ require 'mkmf'
 require 'rb_sys/mkmf'
 
 create_rust_makefile('stateset_embedded/stateset_embedded') do |rust|
-  rust.ext_dir = '../..'
+  rust.ext_dir = '/../..'
   rust.features = %w[runtime]
 end
